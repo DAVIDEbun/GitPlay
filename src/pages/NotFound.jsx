d9f44ac6551a52c5d555c6742f7c58b44f9f4742
DAVIDEbun <ebunoluwadamilare@gmail.com>
2023-01-26 17:41:35 +0100

    clean up

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 
 function NotFound() {
-  return <div className="errorPage">NotFound</div>;
+  return <div className="errorPage"><h1>NotFound</h1></div>;
 }
 
 export default NotFound;
